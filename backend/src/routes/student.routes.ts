408ef52f47a9aeb01cc721c1590ecebc0308065c
TawodzeraT <tavongatawodzera6@gmail.com>
2026-05-20 23:56:11 -0400

    Add route for getting courses in student.routes.ts

--- backend/src/routes/student.routes.ts
+++ backend/src/routes/student.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { getDashboard } from '../controllers/student.controller';
 import { protect } from '../middleware/auth.middleware';
+import { getDashboard, getMyCourses } from '../controllers/student.controller';
 
+router.get('/courses', getMyCourses);
 const router = Router();
 
 router.use(protect);
