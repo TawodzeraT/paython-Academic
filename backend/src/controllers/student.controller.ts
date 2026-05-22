55f297ec2d490498f16c8c203572f473f0791876
TawodzeraT <tavongatawodzera6@gmail.com>
2026-05-21 23:10:07 -0400

    Fix import statement for Request in student.controller

--- backend/src/controllers/student.controller.ts
+++ backend/src/controllers/student.controller.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import { Response } from 'express';
 import { prisma } from '../utils/prisma';
 import { AuthRequest } from '../middleware/auth.middleware';
